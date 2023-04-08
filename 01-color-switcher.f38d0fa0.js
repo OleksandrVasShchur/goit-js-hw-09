c608f8055294e626b7ec80f9ad04d4b7d52f5998
OleksandrVasShchur <102593870+OleksandrVasShchur@users.noreply.github.com>
2023-04-08 21:41:22 +0000

    Deploying to gh-pages from @ OleksandrVasShchur/goit-js-hw-09@a9f3125a28a6bc359552751389b8b380dbca1544 🚀

--- 01-color-switcher.f38d0fa0.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.setAttribute("disabled",!0),t.addEventListener("click",(function(){t.setAttribute("disabled",!0),setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),console.log(o)}),1e3),e.setAttribute("disabled",!1)}))}();
-//# sourceMappingURL=01-color-switcher.f38d0fa0.js.map
