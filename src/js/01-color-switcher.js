6cf64572133ba7fab9c2e9bfa5da87606858ba05
Oleksandr Shchur <oleksandrschur7@gmail.com>
2023-04-09 00:20:35 +0300

    start

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,37 @@
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  let changeColors = null;
+
+  const buttonStart = document.querySelector("button[data-start]");
+  const buttonStop = document.querySelector("button[data-stop]");
+ const bodyColor = document.querySelector("body");
+buttonStart.setAttribute('disabled', false);
+buttonStop.setAttribute("disabled", true);
+
+  buttonStart.addEventListener("click", onClickStart);
+  
+ 
+function onClickStart() { 
+ buttonStart.setAttribute("disabled", true);
+ buttonStop.setAttribute("disabled", false);
+changeColors = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor()
+    console.log(bodyColor);
+   
+  }, 1000);
+ 
+}
+
+
+
+function onClickStop() {
+  buttonStart.setAttribute("disabled", false);
+  buttonStop.setAttribute("disabled", true);
+  buttonStop.addEventListener("click", () => {
+    clearInterval(changeColors);
+  });
+  
+}
+
