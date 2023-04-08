4c9633fdb18bb3d3d9dc18fbdc157c1f99dad386
Oleksandr Shchur <oleksandrschur7@gmail.com>
2023-04-09 01:08:45 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,7 +7,7 @@ function getRandomHexColor() {
   const buttonStart = document.querySelector("button[data-start]");
   const buttonStop = document.querySelector("button[data-stop]");
  const bodyColor = document.querySelector("body");
-// buttonStop.setAttribute("disabled", true);
+buttonStop.setAttribute("disabled", true);
 
   buttonStart.addEventListener("click", onClickStart);
 
@@ -15,7 +15,7 @@ function getRandomHexColor() {
  
 function onClickStart() { 
  buttonStart.setAttribute("disabled", true);
-//  buttonStop.setAttribute("disabled", false);
+ buttonStop.removeAttribute("disabled");
 changeColors = setInterval(() => {
     bodyColor.style.backgroundColor = getRandomHexColor()
     console.log(bodyColor);
@@ -29,7 +29,7 @@ function onClickStop() {
   
   // buttonStop.setAttribute("disabled", true);
   clearInterval(changeColors)
-  
-  buttonStart.setAttribute("disabled", false);
+
+  buttonStart.removeAttribute("disabled");
 }
 
