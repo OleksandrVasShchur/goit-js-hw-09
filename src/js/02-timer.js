29cfcd23b740ec67a9ef46af437bc608a515931c
Oleksandr Shchur <oleksandrschur7@gmail.com>
2023-04-09 23:52:10 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,33 +2,31 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 
-// const deadLine = new Date(2023, 7, 5);
-
 const dayElement = document.querySelector("[data-days]");
 const hourElement = document.querySelector("[data-hours]");
 const minuteElement = document.querySelector("[data-minutes]");
 const secondElement = document.querySelector("[data-seconds]");
 
-let deadLine = null; 
+let deadLine = new Date(2023, 7, 5); 
 
 
 
 const options = {
   enableTime: true,
   time_24hr: true,
+  noCalendar: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  minDate: "today",
   onClose(selectedDates) {
-deadLine = selectedDates;
+// deadLine = selectedDates[0];
     console.log(selectedDates[0]);
-    if(selectedDates <= dateToday){
-      alert("no work");
-    }
+  
   },
 };
 flatpickr("#datetime-picker", {options});
 console.log("lllkjsdfas;dkljfn");
-
+console.log(options);
 
 
 
@@ -36,29 +34,11 @@ console.log("lllkjsdfas;dkljfn");
 function timer() {
 
   const dateToday = new Date();
-  const time = dateToday - deadLine;
+  const time = deadLine - dateToday ;
   console.log(time);
-  
-  function convertMs(time) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(time / day);
-    // Remaining hours
-    const hours = Math.floor((time % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((time % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((time % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-    
+  convertMs(time);
+  return;
   }
-}
 setInterval(timer, 1000);
 
 // dayElement.textContent = days;
@@ -72,7 +52,24 @@ setInterval(timer, 1000);
 
 
 
+function convertMs(time) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  // Remaining days
+  const days = Math.floor(time / day);
+  // Remaining hours
+  const hours = Math.floor((time % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((time % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((time % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
 
 
 
