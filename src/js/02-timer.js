4fe08506876a96d3b275d636f4ca87b69352799c
Oleksandr Shchur <oleksandrschur7@gmail.com>
2023-04-09 23:11:49 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -20,7 +20,8 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
 deadLine = selectedDates;
-    console.log(selectedDates[0]);
+    console.log(selectedDates);
+    
   },
 };
 flatpickr("#datetime-picker", {options});
@@ -29,39 +30,38 @@ flatpickr("#datetime-picker", {options});
 
 
 
-function timer() {
+// function timer() {
 
-  const dateToday = new Date();
-  const time = dateToday - deadLine;
-  console.log(time);
+//   const dateToday = new Date();
+//   const time = dateToday - deadLine;
+//   console.log(time);
   
-  // function convertMs(time) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+//   function convertMs(time) {
+//     // Number of milliseconds per unit of time
+//     const second = 1000;
+//     const minute = second * 60;
+//     const hour = minute * 60;
+//     const day = hour * 24;
   
-    // Remaining days
-    const days = Math.floor(time / day);
-    // Remaining hours
-    const hours = Math.floor((time % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((time % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((time % day) % hour) % minute) / second);
+//     // Remaining days
+//     const days = Math.floor(time / day);
+//     // Remaining hours
+//     const hours = Math.floor((time % day) / hour);
+//     // Remaining minutes
+//     const minutes = Math.floor(((time % day) % hour) / minute);
+//     // Remaining seconds
+//     const seconds = Math.floor((((time % day) % hour) % minute) / second);
 
-
-    dayElement.textContent = days;
-    hourElement.textContent = hours;
-    minuteElement.textContent = minutes;
-    secondElement.secondElement = seconds;
-   
-    return { days, hours, minutes, seconds };
-  }
+//     return { days, hours, minutes, seconds };
+    
+//   }
 // }
-setInterval(timer, 1000);
+// setInterval(timer, 1000);
 
+// dayElement.textContent = days;
+// hourElement.textContent = hours;
+// minuteElement.textContent = minutes;
+// secondElement.secondElement = seconds;
 // console.log(convertMs(time)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 
 
